bc717eef67da34d4984a733982b1ea5f29d61713
Ashish Jha <ashishxyzjha@gmail.com>
2025-10-02 17:32:34 +0530

    HomePage Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { IconArrowUpRight } from "@tabler/icons-react";
+import { MdArrowForwardIos } from "react-icons/md";
 
 export default function HomePage() {
   return (
     <div className="w-full min-h-screen bg-black">
-      <nav className="max-w-5xl mx-4 sm:mx-auto h-fit z-10 flex items-center justify-between px-5 py-2.5 sm:px-8 sm:py-3 mt-4 rounded-sm backdrop-blur-sm bg-teal-300/25">
+      <nav className="max-w-4xl mx-4 sm:mx-auto h-fit z-10 flex items-center justify-between px-5 py-2.5 sm:px-8 sm:py-3 mt-4 rounded-md backdrop-blur-sm bg-teal-300/25">
         <a href="/" className="cursor-pointer">
           <Image src="/icon.svg" alt="Logo" className="w-8 h-8 sm:w-11 sm:h-11" width={40} height={40} />
         </a>
@@ -17,22 +18,54 @@ export default function HomePage() {
       </nav>
 
       <main className="flex flex-col items-center justify-center pt-15">
-      <h1 className="text-4xl md:text-5xl font-bold text-gray-800 text-center">
-        JEE Exam Practice
-      </h1>
-      <p className="mt-4 text-lg text-gray-600 text-center">
-        Boost your preparation with free mock questions
-      </p>
+        <h1 className="text-3xl md:text-5xl font-bold text-white opacity-90 text-center">
+          JEE Exam Practice
+        </h1>
+        <p className="mt-4 text-md sm:text-lg text-white opacity-60 text-center px-20">
+          Boost your preparation with free mock questions
+        </p>
 
-      {/* CTA Button */}
-      <Link
-        href="/dashboard"
-        className="mt-8 px-6 py-3 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
-      >
-        Start Practicing →
-      </Link>
-    </main>
+        <div className="w-fit h-fit border border-white/30 p-1.5 my-10">
+          <div className="border border-dashed border-teal-300/50 px-4 sm:px-8 py-2 sm:py-4">
+            <h1 className="text-white text-lg sm:text-2xl font-medium">Personalized Recommendations</h1>
+            <p className="text-white opacity-70 text-md sm:text-lg font-medium">Get personalized questions that help you.</p>
+            
+            <div className="mt-8 px-5 py-3 rounded-2xl bg-neutral-800 hover:bg-neutral-800/95 w-full h-fit hover:scale-105 transition-all duration-400 cursor-pointer">
+              <div className="flex gap-3 items-center">
+                <div>
+                  <Image src="/jee-main.png" alt="JEE Main" className="w-10 h-10" width={40} height={40} />
+                </div>
+                <div className="text-white">
+                  <h1 className="font-bold tracking-wide">Take Mock Test</h1>
+                  <p className="opacity-70 tracking-wide">JEE Main</p>
+                </div>
+              </div>
+            </div>
 
+            <div className="mt-4 px-5 py-3 rounded-2xl bg-neutral-800 hover:bg-neutral-800/95 w-full h-fit hover:scale-105 transition-all duration-400 cursor-pointer">
+              <div className="flex gap-3 items-center">
+                <div>
+                  <Image src="/jee-advanced.png" alt="Jee Advanced" className="w-10 h-10" width={40} height={40} />
+                </div>
+                <div className="text-white">
+                  <h1 className="font-bold tracking-wide">Practice Mock Test</h1>
+                  <p className="opacity-70 tracking-wide">JEE Advance</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <Link href="/dashboard" className="px-6 py-3 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 group">
+          <div className="flex gap-2 items-center">
+            <span>Start Practicing</span>
+            <span className="transition-transform duration-400 group-hover:translate-x-2">
+              <MdArrowForwardIos />
+            </span>
+          </div>
+        </Link>
+      </main>
 
     </div>
   )
